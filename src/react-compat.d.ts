853d9522558fab5c7149717343ebb08cc77bc77d
Ashiq <108174129+Ashii1@users.noreply.github.com>
2026-09-16 07:32:39 +0530

    fix: restore React Component props typing

--- src/react-compat.d.ts
+++ src/react-compat.d.ts
@@ -2,3 +2,9 @@ declare namespace React {
   type ReactNode = import('react').ReactNode;
   type ChangeEvent<T = Element> = import('react').ChangeEvent<T>;
 }
+
+declare module 'react' {
+  interface Component<P = {}, S = {}, SS = any> {
+    readonly props: P;
+  }
+}
