1feb0e82611be55cb4fc915e58e44f66d8c983b0
Ashiq <108174129+Ashii1@users.noreply.github.com>
2026-09-16 07:33:38 +0530

    fix: keep React compatibility declarations non-invasive

--- src/react-compat.d.ts
+++ src/react-compat.d.ts
@@ -2,9 +2,3 @@ declare namespace React {
   type ReactNode = import('react').ReactNode;
   type ChangeEvent<T = Element> = import('react').ChangeEvent<T>;
 }
-
-declare module 'react' {
-  interface Component<P = {}, S = {}, SS = any> {
-    readonly props: P;
-  }
-}
