18f77fcea7e1cfe0015353d24bc20d82d8b6c795
Ashiq <108174129+Ashii1@users.noreply.github.com>
2026-09-19 00:54:48 +0530

    brand: use CallShield icon resource

--- app/src/main/java/com/vigilshield/telecom/MainActivity.kt
+++ app/src/main/java/com/vigilshield/telecom/MainActivity.kt
@@ -253,7 +253,7 @@ class MainActivity : AppCompatActivity() {
     private fun createLoadingView(): View = FrameLayout(this).apply {
         setBackgroundColor(Color.rgb(2, 6, 23))
         val icon = android.widget.ImageView(this@MainActivity).apply {
-            setImageResource(R.drawable.ic_vigilshield)
+            setImageResource(R.drawable.ic_callshield)
             scaleType = android.widget.ImageView.ScaleType.FIT_CENTER
             contentDescription = "CallShield"
             alpha = 0f
